a006a1d60318f3bb7745d01b688ac162b24bbd79
Bach <batr.schip@gmail.com>
2019-04-28 17:45:56 -0400

    added Comparator functionality

--- src/main/java/org/grasple/backend/entity/Vertex.java
+++ src/main/java/org/grasple/backend/entity/Vertex.java
@@ -1,22 +1,25 @@
 package org.grasple.backend.entity;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.Set;
 
 /**
  * <p align = "justify">
  *     Vertex is a basic element in Graph Theory. Here, a Vertex is a generic class,
- *     strongly associates with a generic entity for convenience. It is strongly
- *     recommended that the generic entity should be comparable.
+ *     strongly associates with a generic entity for convenience.
+ *     The Vertex must have a value during initialization, and that value should
+ *     be Comparable, to make other procedures on the graph become meaningful.
  * </p>
  * @author Bach Tran
  * @since 1.0
- * @param <T> any class comparable
+ * @param <T> any class that is Comparable
  */
 public class Vertex<T extends Comparable<T>> {
     protected T value;
+    /** The set of edges that this vertex has. Currently initialized
+     * to be an empty HashSet.*/
     protected Set<Edge> edges = new HashSet<>();
-    public Vertex() { }
     public Vertex(T value) {
         this.value = value;
     }
@@ -31,7 +34,13 @@ public class Vertex<T extends Comparable<T>> {
         return value;
     }
 
+    /**
+     * Modifies the value of this vertex.
+     * @Contract value != null;
+     * @param value the new value to be changed.
+     */
     public void setValue(T value) {
+        assert value != null;
         this.value = value;
     }
 
@@ -39,11 +48,29 @@ public class Vertex<T extends Comparable<T>> {
         return edges;
     }
 
+    public static Comparator<Vertex> valueComparator = Comparator.comparing(o -> o.value);
+
+    /**
+     * Connects this vertex with another vertex, by creating
+     * a new, weightless edge.
+     * <b>Note:</b> the edge created will be added to both
+     * this vertex, and the other vertex it connects to.
+     * @param other the other vertex to be connected to this vertex.
+     */
     public void connect(Vertex other) {
         Edge edge = new Edge(this, other);
         this.edges.add(edge);
         other.edges.add(edge);
     }
+
+    /**
+     * Connects this vertex with another vertex, by creating
+     * a new edge having a specific weight.
+     * <b>Note:</b> the edge created will be added to both
+     * this vertex, and the other vertex it connects to.
+     * @param weight weight of the edge.
+     * @param other the other vertex to be connected to this vertex.
+     */
     public void connect(int weight, Vertex other) {
         Edge edge = new Edge(weight, this, other);
         this.edges.add(edge);
