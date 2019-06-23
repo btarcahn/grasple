9c38168e60e96a2f94700017ec248f9c7cb80c22
Bach Tran <batr.schip@gmail.com>
2019-06-24 00:37:00 +0700

    cleared BinaryTree logics

--- src/main/java/org/grasple/api/structures/trees/BinaryTree.java
+++ src/main/java/org/grasple/api/structures/trees/BinaryTree.java
@@ -1,9 +1,6 @@
 package org.grasple.api.structures.trees;
 
-import org.grasple.api.particles.Connectable;
-import org.grasple.api.particles.IndexSaturatedException;
-import org.grasple.api.particles.BoundedVertex;
-import org.grasple.api.particles.OrderedVertex;
+import org.grasple.api.particles.*;
 
 
 /**
@@ -16,58 +13,16 @@ import org.grasple.api.particles.OrderedVertex;
  * @author Bach Tran
  */
 public class BinaryTree<T extends Comparable<T>> extends Tree<T> {
-    private static final short LEFT = 0;
-    private static final short RIGHT = 1;
-    private static final short BIN_BOUND = 2;
-
-    public BinaryTree(T value) {
-        super(new BoundedVertex<>(value, BIN_BOUND));
-    }
-
-    public BinaryTree(BoundedVertex<T> root) {
-        super(root);
-    }
-
-    public boolean add(T value) {
-        assert this.getRoot() instanceof BoundedVertex;
-        _add((BoundedVertex<T>) getRoot(), value);
-        return false;
-    }
-
-    private boolean _add(BoundedVertex<T> vertex, T value) {
-        // TODO check the preconditions of vertex
-        // add to this vertex
-        if (value.compareTo(vertex.get()) < 0) {
-            try {
-                vertex.connect(createBinVertex(value), LEFT);
-            } catch (IndexSaturatedException e) {
-                assert vertex.getNeighbor(LEFT) != null;
-                return _add(vertex.getNeighbor(LEFT), value);
-            }
-            return true;
-        } else {
-            try {
-                vertex.connect(createBinVertex(value), RIGHT);
-            } catch (IndexSaturatedException e) {
-                assert vertex.getNeighbor(RIGHT) != null;
-                return _add(vertex.getNeighbor(RIGHT), value);
-            }
-            return true;
-        }
-    }
-
-    private BoundedVertex<T> createBinVertex(T value) {
-        return new BoundedVertex<>(value, BIN_BOUND);
-    }
+    private static final int LEFT = 0;
+    private static final int RIGHT = 1;
+    private static final int BIN_BOUND = 2;
 
     /**
-     * A connected object is saturated if they already own
-     * two connections. This method checks whether the given
-     * connectable object is saturated or not.
-     * @param connectable the connectable object to be checked for saturation
-     * @return true if the connectable is saturated.
+     * Creates a connected graph with at least one root vertex.
+     *
+     * @param root the root vertex of the tree.
      */
-    private static boolean saturated(Connectable connectable) {
-        return connectable.getNeighbors().size() >= 2;
+    public BinaryTree(Connectable<T> root) {
+        super(root);
     }
 }
